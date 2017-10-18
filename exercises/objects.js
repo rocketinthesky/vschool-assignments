60011c8c89ae7d611918c51c680d884a7fe6e697
Neil Wagner <aang@Neils-MacBook-Pro.local>
2017-10-18 09:22:31 -0600

    updating yesterdays exercises

---
+++ exercises/objects.js
@@ -0,0 +1,32 @@
+// Create three objects of things you see in the room.
+//
+// The objects must have four properties.
+//
+// Between the three objects, you will include properties that are four different data types.
+//
+// example:
+//
+// var monitor = {}
+// var chair = {}
+// var classMate = {}
+
+var monitor = {
+  weightInLbs: 7,
+  isBorrowed: true,
+  brandName: 'Asus',
+  alternativeBrands: ['LG', 'Samsung', 'Dell']
+}
+
+var laptop = {
+  weightInLbs: 4,
+  isBorrowed: false,
+  brandName: 'Apple',
+  alternativeBrands: ['Alienware', 'Microsoft', 'Asus']
+}
+
+var mouse = {
+  weightInLbs: 1,
+  isBorrowed: false,
+  brandName: 'Apple',
+  alternativeBrands: ['Logitech', 'Razer', 'Microsoft']
+}
